97b521db6deddb9612230a691ed6cf9441d10a3d
Sam Trettin <trettinftw@gmail.com>
2019-01-05 20:26:26 -0800

    Updates

---
+++ precache-manifest.cab03a86653dc51fab1d0c7324788a45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bdf6617a7b635f349ba",
+    "url": "/React_Todo/static/js/runtime~main.3bdf6617.js"
+  },
+  {
+    "revision": "63b3251fdde5a3db2b76",
+    "url": "/React_Todo/static/js/main.63b3251f.chunk.js"
+  },
+  {
+    "revision": "6f96e78ba8e11d79daff",
+    "url": "/React_Todo/static/js/1.6f96e78b.chunk.js"
+  },
+  {
+    "revision": "63b3251fdde5a3db2b76",
+    "url": "/React_Todo/static/css/main.5b3f8e03.chunk.css"
+  },
+  {
+    "revision": "6277061b710a10552ac857726c8eb8a2",
+    "url": "/React_Todo/index.html"
+  }
+];
